676743549124c812eb571ea7aeeaa694179f3450
Lina Petrovsky <linapetrovsky@gmail.com>
2024-11-23 18:31:35 +0200

    add logo assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import FlightList from "./components/FlightList";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <FlightList />
-    </>
+    <div>
+      <Header />
+      {/* <FlightList /> */}
+      <Footer />
+    </div>
   );
 }
 
