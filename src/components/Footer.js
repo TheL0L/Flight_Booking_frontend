ecb726285d40a3d37b8fc6936c3dd2eff0f89f14
linapetrovsky <118122434+linapetrovsky@users.noreply.github.com>
2024-11-24 15:14:52 +0200

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer className="bg-dark text-white text-center p-3">
-                <p>&copy; 2021 Flight Booking System</p>
+                <p>&copy; 2024 Final Project: Intro to DevOps - Hello World Flight Bookings</p>
             </footer>
         );
     }
