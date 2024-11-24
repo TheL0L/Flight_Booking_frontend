4b060674bff323e15dd1d08dd4cb67bb4f9d02d6
linapetrovsky <118122434+linapetrovsky@users.noreply.github.com>
2024-11-24 15:50:39 +0200

    added pics to home carousel

--- src/components/CarouselBox.js
+++ src/components/CarouselBox.js
@@ -3,6 +3,8 @@ import Carousel from "react-bootstrap/Carousel";
 import CarouselItem from "react-bootstrap/CarouselItem";
 import palmsImg from "../assets/palms.jpg";
 import mountainsImg from "../assets/mountains.jpg";
+import celebrateImg from "../assets/celebrate.jpg";
+import loveImg from "../assets/love.jpg";
 
 export default class CarouselBox extends Component {
     render() {
@@ -15,8 +17,7 @@ export default class CarouselBox extends Component {
                         alt="Mountains"
                     />
                     <Carousel.Caption>
-                        <h3>Home</h3>
-                        <p>Hello World Bookings is a company that provides you with the best flight booking services. We offer the best prices and the best customer service. We are here to make your flight booking experience as easy as possible.</p>
+                        <h3>Travel</h3>
                     </Carousel.Caption> 
                 </CarouselItem>
                 <CarouselItem>
@@ -26,8 +27,27 @@ export default class CarouselBox extends Component {
                         alt="Palms"
                     />
                     <Carousel.Caption>
-                        <h3>Home</h3>
-                        <p>Hello World Bookings is a company that provides you with the best flight booking services. We offer the best prices and the best customer service. We are here to make your flight booking experience as easy as possible.</p>
+                        <h3>Explore</h3>
+                    </Carousel.Caption>
+                </CarouselItem>
+                <CarouselItem>
+                    <img
+                        className="d-block w-100"
+                        src={ celebrateImg }
+                        alt="Celebrate"
+                    />
+                    <Carousel.Caption>
+                        <h3>Celebrate</h3>
+                    </Carousel.Caption>
+                </CarouselItem>
+                <CarouselItem>
+                    <img
+                        className="d-block w-100"
+                        src={ loveImg }
+                        alt="Love"
+                    />
+                    <Carousel.Caption>
+                        <h3>Love</h3>
                     </Carousel.Caption>
                 </CarouselItem>
             </Carousel>
